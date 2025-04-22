c30db8e59de91803d0f9b661dafcba4b334661e5
TuanNguyen-Code <conheosaumui2005@gmail.com>
2025-04-22 22:32:28 +0700

    Lession-01

---
+++ Lession-01-HomeWork/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lession_01_HomeWork.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult ProductView()
+        {
+            return View();
+        }
+    }
+}
---
+++ Lession-01-HomeWork/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lession_01_HomeWork.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
