ae71e19e4b3e0b2d3bab6b89398c4672c23b2a80
TuanNguyen-Code <conheosaumui2005@gmail.com>
2025-05-13 11:07:45 +0700

    Lession04

---
+++ Lession04/Controllers/BooksController.cs
@@ -0,0 +1,19 @@
+﻿using Lession04.Models;
+using Microsoft.AspNetCore.Mvc;
+using Humanizer;
+
+namespace Lession04.Controllers
+{
+    public class BooksController : Controller
+    {
+        protected Book book = new Book();
+        public IActionResult NmitIndex()
+        {
+            ViewBag.authors = book.Authors;
+            ViewBag.genres = book.Genres;
+
+            var books = book.GetBookList();
+            return View(books);
+        }
+    }
+}
